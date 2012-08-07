6bf5cc91a6bd2d4292c4eb362964f9ca8d81b85e
Nitesh <nitesh@Nitesh.(none)>
2012-08-07 18:35:23 +0530

    Aeolus GUI Initial Configuration Module Implementation

--- InitialConfiguration.cpp
+++ InitialConfiguration.cpp
@@ -1,9 +1,27 @@
 #include"InitialConfiguration.h"
+#include<QFile>
+#include<QTextStream>
+#include <iostream>
+
 InitialConfiguration::InitialConfiguration()
 {
     setupUi(this);
     this->setWindowTitle("Aeolus Initial Configuration");
-    system("gcc command.c -o command");
-    system("chmod a+x command");
-    system("./command");
+    system("echo $DESKTOP_SESSION>desktopEnvironment.txt");
+    QFile file("desktopEnvironment.txt");
+    file.open(QIODevice::Text | QIODevice::ReadOnly);
+    QTextStream out(&file);
+    QString environment;
+    while(!out.atEnd())
+    {
+        environment=out.readLine();
+    }
+    QByteArray environmentEA = environment.toLocal8Bit();
+    const char *environmentC = environmentEA.data();
+    if(strstr(environmentC,"kde"))
+    std::cout << system("kdesu sh command.sh");
+    else
+    std::cout << system("gnomesu sh command.sh");
+    file.remove();
 }
+
--- command.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-void main()
-{
-system("/usr/sbin/aeolus-configure -p ec2");
-system("/usr/sbin/aeolus-configure -p mock");
-system("yes n | cp iptables /etc/sysconfig ");
-system("yes n | cp ip6tables /etc/sysconfig ");  
-}

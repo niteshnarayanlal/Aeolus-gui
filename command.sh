30b307d534be12ba028e59fe90fe1105cdece1df
Nitesh <nitesh@Nitesh.(none)>
2012-08-07 18:37:20 +0530

    Aeolus GUI Intial Config module

---
+++ command.sh
@@ -0,0 +1,8 @@
+/usr/sbin/aeolus-configure -p ec2
+/usr/sbin/aeolus-configure -p mock
+yes n | cp iptables /etc/sysconfig 
+yes n | cp ip6tables /etc/sysconfig 
+#cp /home/nitesh/Programming/Aeolus/aeolus.pro /home/nitesh/
+#chmod 777 /home/nitesh/aeolus.pro
+
+#pwd > /home/nitesh/abc
